fde42fe047ce85cf5c28479cfcfd2837f21fcc60
Thor Dreier-Hansen <43479811+thordreier@users.noreply.github.com>
2020-11-27 21:50:38 +0100

    Fix leading zeroes in LPFReceiveIRValues.ino

--- examples/LPFReceiveIRValues/LPFReceiveIRValues.ino
+++ examples/LPFReceiveIRValues/LPFReceiveIRValues.ino
@@ -15,8 +15,16 @@ void setup() {
 }
 
 void loop() {
-  while( lpf_msg msg = lpf_receiver_get_next_msg() )
-  {
-    Serial.println( msg, BIN );
+  while( lpf_msg msg = lpf_receiver_get_next_msg() ) {
+
+    // If using  this, then leading zeroes will not be printed
+    //Serial.println( msg, BIN );
+
+    // Using this will always print all 16 bits
+    for( int i = 15; i >= 0; i--) {
+      Serial.print((msg >> i) & 1 == 1 ? "1" : "0");
+    }
+    Serial.println();
+
   }
 }
