e7a68eb02ffe541c42776bfa96d748603f67424d
Thor Dreier-Hansen <43479811+thordreier@users.noreply.github.com>
2020-12-17 08:56:44 +0100

    use sprintf() in LPFMotorDriverReceiverSerial.ino

--- examples/LPFMotorDriverReceiverSerial/LPFMotorDriverReceiverSerial.ino
+++ examples/LPFMotorDriverReceiverSerial/LPFMotorDriverReceiverSerial.ino
@@ -10,29 +10,16 @@ byte channel = 3;  // between 0 and 3 (for channel 1-4)
 
 LpfDriver driver(channel);
 
-void printNumber(uint8_t i) {
-  Serial.print(i < 10 ? "  " : i < 100 ? " " : "");
-  Serial.print(i);
-}
-
-void printSpeed(int8_t speed, byte brake) {
-  uint16_t pwm = lpfConvertSpeed(speed);
-  Serial.print(speed > 0 ? "+" : speed ? "" : " ");
-  Serial.print(speed);
-  Serial.print(" PWM(");
-  printNumber((uint8_t)(pwm >> 8));
-  Serial.print(",");
-  printNumber((uint8_t)(pwm));
-  Serial.print(") ");
-  Serial.print(brake ? "B" : " ");
-}
-
 void controlMotor(int8_t speed0, byte brake0, int8_t speed1, byte brake1) {
-  Serial.print("Red speed: ");
-  printSpeed(speed0, brake0);
-  Serial.print(" / Blue speed: ");
-  printSpeed(speed1, brake1);
-  Serial.println();
+  char str[255];
+  sprintf(
+      str,
+      "Red speed: %+2i PWM(%3i,%3i) %s / Blue speed: %+2i PWM(%3i,%3i) %s\n",
+      speed0, (uint8_t)(lpfConvertSpeed(speed0) >> 8),
+      (uint8_t)(lpfConvertSpeed(speed0)), brake0 ? "B" : " ", speed1,
+      (uint8_t)(lpfConvertSpeed(speed1) >> 8),
+      (uint8_t)(lpfConvertSpeed(speed1)), brake1 ? "B" : " ");
+  Serial.print(str);
 }
 
 void setup() {
